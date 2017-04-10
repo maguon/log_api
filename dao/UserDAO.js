303ed8db8107f3585784e22d40191cfb555efdaf
zwl123 <wenlong.zhang@myxxjs.com>
2017-04-10 16:53:24 +0800

    更新usertype

--- dao/UserDAO.js
+++ dao/UserDAO.js
@@ -7,12 +7,12 @@ var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('UserDAO.js');
 
 function addUser(params,callback){
-    var query = " insert into user_info (mobile,real_name,password,dept_id,gender) values ( ? , ? , ? , ? , ? )";
+    var query = " insert into user_info (mobile,real_name,password,type,gender) values ( ? , ? , ? , ? , ? )";
     var paramsArray=[],i=0;
     paramsArray[i++]=params.mobile;
     paramsArray[i++]=params.realName;
     paramsArray[i++]=params.password;
-    paramsArray[i++]=params.deptId;
+    paramsArray[i++]=params.type;
     paramsArray[i++]=params.gender;
     db.dbQuery(query,paramsArray,function(error,rows){
         logger.debug(' addUser ');
@@ -21,7 +21,7 @@ function addUser(params,callback){
 }
 
 function getUser(params,callback) {
-    var query = " select u.*,d.dept_name from user_info u left join department d on u.dept_id = d.dept_id where uid is not null ";
+    var query = " select * from user_info where uid is not null ";
     var paramsArray=[],i=0;
     if(params.userId){
         paramsArray[i++] = params.userId;
@@ -47,9 +47,7 @@ function getUser(params,callback) {
 }
 
 function getUserBase(params,callback){
-    var query = " select u.uid,u.mobile,u.real_name,u.password,u.dept_id,u.gender," +
-        " u.status,d.dept_name from user_info u left join department d on u.dept_id = d.dept_id " +
-        " where uid is not null ";
+    var query = " select uid,mobile,real_name,password,type,gender,status from user_info where uid is not null ";
     var paramsArray=[],i=0;
     if(params.userId){
         paramsArray[i++] = params.userId;
@@ -75,10 +73,10 @@ function getUserBase(params,callback){
 }
 
 function updateUserInfo(params,callback){
-    var query = " update user_info set real_name = ? , dept_id = ? , gender = ? , mobile = ? ,status = ?  where uid = ?";
+    var query = " update user_info set real_name = ? , type = ? , gender = ? , mobile = ? ,status = ?  where uid = ?";
     var paramsArray=[],i=0;
     paramsArray[i++] = params.realName;
-    paramsArray[i++] = params.deptId;
+    paramsArray[i++] = params.type;
     paramsArray[i++] = params.gender;
     paramsArray[i++] = params.mobile;
     paramsArray[i++] = params.status;
