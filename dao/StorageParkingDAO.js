ee274306fc6c91bca25c145b6944803426b13895
lingxue <info@myxxjs.com>
2017-04-14 17:16:05 +0800

    add storage parking create api

--- dao/StorageParkingDAO.js
+++ dao/StorageParkingDAO.js
@@ -9,7 +9,7 @@ var logger = serverLogger.createLogger('StorageParkingDAO.js');
 function addStorageParking(params,callback){
     var query = " insert into storage_parking (storage_id,row,col) values ( ? , ? , ? ) ";
     var paramsArray=[],i=0;
-    paramsArray[i++]=params.storage_id;
+    paramsArray[i++]=params.storageId;
     paramsArray[i++]=params.row;
     paramsArray[i]=params.col;
     db.dbQuery(query,paramsArray,function(error,rows){
