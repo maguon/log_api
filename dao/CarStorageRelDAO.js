021a42e5345416eb20f6aa1cac7ad62922718539
zwl123 <wenlong.zhang@myxxjs.com>
2017-04-19 15:58:29 +0800

    更新CarStorageRelDAO.js

--- dao/CarStorageRelDAO.js
+++ dao/CarStorageRelDAO.js
@@ -21,6 +21,31 @@ function addCarStorageRel(params,callback){
     });
 }
 
+function getCarStorageRel(params,callback) {
+    var query = " select r.* from car_storage_rel r left join storage_parking p on r.car_id = p.car_id where r.id is not null ";
+    var paramsArray=[],i=0;
+    if(params.relId){
+        paramsArray[i++] = params.relId;
+        query = query + " and r.id = ? ";
+    }
+    if(params.storageId){
+        paramsArray[i++] = params.storageId;
+        query = query + " and r.storage_id = ? ";
+    }
+    if(params.carId){
+        paramsArray[i++] = params.carId;
+        query = query + " and r.car_id = ? ";
+    }
+    if(params.relStatus){
+        paramsArray[i++] = params.relStatus;
+        query = query + " and r.rel_status = ? ";
+    }
+    db.dbQuery(query,paramsArray,function(error,rows){
+        logger.debug(' getCarStorageRel ');
+        return callback(error,rows);
+    });
+}
+
 function updateRelStatus(params,callback){
     var query = " update car_storage_rel set real_out_time = sysdate() , rel_status = ? where id = ? ";
     var paramsArray=[],i=0;
@@ -35,5 +60,6 @@ function updateRelStatus(params,callback){
 
 module.exports ={
     addCarStorageRel : addCarStorageRel,
+    getCarStorageRel : getCarStorageRel,
     updateRelStatus : updateRelStatus
 }
