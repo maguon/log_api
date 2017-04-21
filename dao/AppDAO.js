ab7e020bc8b83230bb7c9bacd56c4be91a8fc11a
lingxue <info@myxxjs.com>
2017-04-21 16:15:49 +0800

    update app api

--- dao/AppDAO.js
+++ dao/AppDAO.js
@@ -3,7 +3,7 @@ var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('AppDAO.js');
 
 function queryApp(params,callback){
-    var query = " select id,app,type,force_update,url,remark,created_on,updated_on from app_version where id is not null ";
+    var query = " select id,app,type,version,force_update,url,remark,created_on,updated_on from app_version where id is not null ";
     var paramsArray=[],i=0;
     if(params.type){
         query = query + " and type = ? "
