13c50cc872595a6f05f08a271bdeb41f991ca2d7
lingxue <info@myxxjs.com>
2017-04-25 11:31:20 +0800

    update app api

--- bl/SysRecord.js
+++ bl/SysRecord.js
@@ -7,19 +7,28 @@ var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('SysRecord.js');
 
 function saveCarRecord (req,res,next){
-    var params = req.params;
-    console.log(params);
-    var recordParams ={};
-    recordParams.userId = params.userId;
-    recordParams.userType = req.headers['user-type'] || 9;
-    recordParams.username = req.headers['user-name'] || 'admin';
-    recordParams.content = params.carContent;
-    sysRecordDAO.addRecord(req,params,function(error,result){
-        if(error){
-            logger.error('saveCarRecord ' + error.stack);
-        }
+    if(res.success){
+        var params = req.params;
+        console.log(params);
+        var recordParams ={};
+        recordParams.userId = params.userId;
+        recordParams.userType = req.headers['user-type'] || 9;
+        recordParams.username = req.headers['user-name'] || 'admin';
+        recordParams.content = params.carContent;
+        recordParams.carId = params.carId;
+        recordParams.vin = params.vin;
+        sysRecordDAO.addRecord(req,recordParams,function(error,result){
+            if(error){
+                logger.error('saveCarRecord ' + error.stack);
+            }else{
+                logger.info('saveCarRecord success')
+            }
+            return next();
+        })
+    }else{
         return next();
-    })
+    }
+
 }
 
 module.exports ={
