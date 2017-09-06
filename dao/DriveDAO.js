93457a5d5896a0a691885c10e6854bb7b23656c6
zwl123 <wenlong.zhang@myxxjs.com>
2017-09-06 09:57:53 +0800

    修改查询司机功能，增加副驾的车牌号

--- dao/DriveDAO.js
+++ dao/DriveDAO.js
@@ -27,9 +27,14 @@ function addDrive(params,callback){
 }
 
 function getDrive(params,callback) {
-    var query = " select d.*,c.company_name,c.operate_type,t.id as truck_id,t.truck_num " +
+/*    var query = " select d.*,c.company_name,c.operate_type,t.id as truck_id,t.truck_num " +
         " from drive_info d left join company_info c on d.company_id = c.id " +
-        " left join truck_info t on d.id = t.drive_id or d.id = t.vice_drive_id where d.id is not null";
+        " left join truck_info t on d.id = t.drive_id or d.id = t.vice_drive_id where d.id is not null";*/
+    var query = " select d.*,ti1.id as truck_id,ti1.truck_num,ti2.id as vice_truck_id,ti2.truck_num as vice,c.company_name,c.operate_type " +
+        " from drive_info d " +
+        " left join truck_info ti1 on d.id=ti1.drive_id " +
+        " left join truck_info ti2 on d.id = ti2.vice_drive_id " +
+        " left join company_info c on d.company_id = c.id where d.id is not null";
     var paramsArray=[],i=0;
     if(params.driveId){
         paramsArray[i++] = params.driveId;
@@ -45,11 +50,11 @@ function getDrive(params,callback) {
     }
     if(params.truckId){
         paramsArray[i++] = params.truckId;
-        query = query + " and t.id = ? ";
+        query = query + " and ti1.id = ? ";
     }
     if(params.truckNum){
         paramsArray[i++] = params.truckNum;
-        query = query + " and t.truck_num = ? ";
+        query = query + " and ti1.truck_num = ? ";
     }
     if(params.gender){
         paramsArray[i++] = params.gender;
