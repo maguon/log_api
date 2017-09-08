c654f826f637fb627a933783363fc3921bbd50ae
zwl123 <wenlong.zhang@myxxjs.com>
2017-09-08 14:48:58 +0800

    添加字段路线ID

--- dao/DriveRefuelDAO.js
+++ dao/DriveRefuelDAO.js
@@ -7,14 +7,15 @@ var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('DriveRefuelDAO.js');
 
 function addDriveRefuel(params,callback){
-    var query = " insert into drive_refuel (drive_id,truck_id,date_id,refuel_date,refuel_volume, " +
-        "refuel_address_type,refuel_address,lng,lat,refuel_money) values ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? ) ";
+    var query = " insert into drive_refuel (drive_id,truck_id,date_id,refuel_date,refuel_volume,city_route_id, " +
+        "refuel_address_type,refuel_address,lng,lat,refuel_money) values ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? ) ";
     var paramsArray=[],i=0;
     paramsArray[i++]=params.driveId;
     paramsArray[i++]=params.truckId;
     paramsArray[i++]=params.dateId;
     paramsArray[i++]=params.refuelDate;
     paramsArray[i++]=params.refuelVolume;
+    paramsArray[i++]=params.cityRouteId;
     paramsArray[i++]=params.refuelAddressType;
     paramsArray[i++]=params.refuelAddress;
     paramsArray[i++]=params.lng;
