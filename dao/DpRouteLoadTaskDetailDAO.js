90f0dbfe713e28860033744326a47726dc85831a
zwl123 <wenlong.zhang@myxxjs.com>
2017-09-12 11:00:08 +0800

    异常vin排序

--- dao/DpRouteLoadTaskDetailDAO.js
+++ dao/DpRouteLoadTaskDetailDAO.js
@@ -41,6 +41,7 @@ function getDpRouteLoadTaskDetail(params,callback) {
         paramsArray[i++] = params.carLoadStatus;
         query = query + " and dpdtl.car_load_status = ? ";
     }
+    query = query + ' order by cer.exception_status ';
     db.dbQuery(query,paramsArray,function(error,rows){
         logger.debug(' getDpRouteLoadTaskDetail ');
         return callback(error,rows);
