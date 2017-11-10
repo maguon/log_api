5b22cca436fdbc699bf4aaf32dfa934ff729943a
zwl123 <wenlong.zhang@myxxjs.com>
2017-11-10 16:53:40 +0800

    添加质损表

--- db/sql/0.0.4/update_base.sql
+++ db/sql/0.0.4/update_base.sql
@@ -5,7 +5,21 @@ ADD COLUMN `arrive_date`  datetime NULL COMMENT '送达时间' AFTER `vin`;
 ADD COLUMN `date_id`  int(4) NULL DEFAULT NULL COMMENT '送达统计时间' AFTER `arrive_date`;
 ALTER TABLE `user_info`
 ADD COLUMN `avatar_image`  varchar(100) NULL COMMENT '用户头像' AFTER `gender`;
-
+ALTER TABLE `dp_task_stat`
+ADD COLUMN `not_plan_count`  int(10) NULL DEFAULT 0 COMMENT '未派发台数' AFTER `plan_count`;
+ADD COLUMN `task_stat_status`  tinyint(1) NOT NULL DEFAULT 1 COMMENT '需求统计状态(1-未完成,2-已完成)' AFTER `date_id`;
+ALTER TABLE `dp_route_load_task`
+ADD COLUMN `field_op_id`  int(10) NULL COMMENT '现场调度员ID' AFTER `user_id`;
+ADD COLUMN `plan_date`  datetime NULL COMMENT '计划装车时间' AFTER `date_id`;
+ADD COLUMN `real_count`  int(10) NULL DEFAULT 0 COMMENT '实际装车数量' AFTER `plan_count`;
+ALTER TABLE `dp_route_load_task_detail`
+ADD COLUMN `dp_route_task_id`  int(10) NULL COMMENT '路线ID' AFTER `id`;
+ALTER TABLE `dp_demand_info`
+ADD COLUMN `plan_count`  int(10) NULL DEFAULT 0 COMMENT '计划派发商品车数量' AFTER `pre_count`;
+ADD COLUMN `not_plan_count`  int(10) NULL DEFAULT 0 COMMENT '未派发台数' AFTER `plan_count`,
+ADD COLUMN `load_count`  int(10) NULL DEFAULT 0 COMMENT '装车数量' AFTER `not_plan_count`;
+ALTER TABLE `receive_info`
+ADD COLUMN `clean_fee`  decimal(10,2) NULL COMMENT '洗车费' AFTER `city_id`;
 -- ----------------------------
 -- Table structure for drive_refuel
 -- ----------------------------
@@ -58,24 +72,11 @@ CREATE TABLE `user_device` (
   `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `dp_task_stat`
-ADD COLUMN `not_plan_count`  int(10) NULL DEFAULT 0 COMMENT '未派发台数' AFTER `plan_count`;
-ADD COLUMN `task_stat_status`  tinyint(1) NOT NULL DEFAULT 1 COMMENT '需求统计状态(1-未完成,2-已完成)' AFTER `date_id`;
-ALTER TABLE `dp_route_load_task`
-ADD COLUMN `field_op_id`  int(10) NULL COMMENT '现场调度员ID' AFTER `user_id`;
-ADD COLUMN `plan_date`  datetime NULL COMMENT '计划装车时间' AFTER `date_id`;
-ADD COLUMN `real_count`  int(10) NULL DEFAULT 0 COMMENT '实际装车数量' AFTER `plan_count`;
-ALTER TABLE `dp_route_load_task_detail`
-ADD COLUMN `dp_route_task_id`  int(10) NULL COMMENT '路线ID' AFTER `id`;
-ALTER TABLE `dp_demand_info`
-ADD COLUMN `plan_count`  int(10) NULL DEFAULT 0 COMMENT '计划派发商品车数量' AFTER `pre_count`;
-ADD COLUMN `not_plan_count`  int(10) NULL DEFAULT 0 COMMENT '未派发台数' AFTER `plan_count`,
-ADD COLUMN `load_count`  int(10) NULL DEFAULT 0 COMMENT '装车数量' AFTER `not_plan_count`;
 -- ----------------------------
--- Table structure for quality_info
+-- Table structure for damage_info
 -- ----------------------------
-DROP TABLE IF EXISTS `quality_info`;
-CREATE TABLE `quality_info` (
+DROP TABLE IF EXISTS `damage_info`;
+CREATE TABLE `damage_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `declare_user_id` int(10) NOT NULL COMMENT '质损申报人',
   `car_id` int(10) NOT NULL COMMENT '商品车ID',
@@ -84,9 +85,82 @@ CREATE TABLE `quality_info` (
   `drive_id` int(10) DEFAULT NULL COMMENT '司机ID',
   `drive_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '司机姓名',
   `date_id` int(4) NOT NULL COMMENT '质损申报统计时间',
-  `quality_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '质损状态(1-待处理,2-处理中,3-已处理)',
-  `quality_explain` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '质损说明',
+  `damage_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '质损状态(1-待处理,2-处理中,3-已处理)',
+  `damage_explain` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '质损说明',
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '质损申报时间',
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- ----------------------------
+-- Table structure for damage_check
+-- ----------------------------
+DROP TABLE IF EXISTS `damage_check`;
+CREATE TABLE `damage_check` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `damage_id` int(10) NOT NULL COMMENT '质损编号ID',
+  `under_user_id` int(10) DEFAULT NULL COMMENT '责任人用户ID',
+  `damage_type` tinyint(1) NOT NULL COMMENT '质损类型(1-一般质损,2-严重质损)',
+  `damage_link_type` tinyint(1) NOT NULL COMMENT '质损环节类别(1-短驳移库,2-公路运输,3-公司运输,4-驾驶员漏检,5-交通事故)',
+  `refund_user_id` int(10) DEFAULT NULL COMMENT '报销人用户ID',
+  `reduction_cost` decimal(10,2) DEFAULT NULL COMMENT '降价费',
+  `penalty_cost` decimal(10,2) DEFAULT NULL COMMENT '罚款',
+  `profit` decimal(10,2) DEFAULT NULL COMMENT '盈亏',
+  `repair_id` int(10) DEFAULT NULL COMMENT '维修站ID',
+  `repair_cost` decimal(10,2) DEFAULT NULL COMMENT '维修费',
+  `transport_cost` decimal(10,2) DEFAULT NULL COMMENT '运费',
+  `under_cost` decimal(10,2) DEFAULT NULL COMMENT '责任人承担费用',
+  `company_cost` decimal(10,2) DEFAULT NULL COMMENT '公司承担费用',
+  `op_user_id` int(10) DEFAULT NULL COMMENT '处理人用户ID',
+  `end_date` datetime DEFAULT NULL COMMENT '处理结束时间',
+  `remark` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '备注',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '质损处理时间',
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- ----------------------------
+-- Table structure for damage_insure
+-- ----------------------------
+DROP TABLE IF EXISTS `damage_insure`;
+CREATE TABLE `damage_insure` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `insure_id` int(10) NOT NULL COMMENT '保险公司ID',
+  `insure_user_id` int(10) NOT NULL COMMENT '经办人',
+  `insure_plan` decimal(10,2) DEFAULT NULL COMMENT '保险计划待赔',
+  `insure_actual` decimal(10,2) DEFAULT NULL COMMENT '保险实际赔付',
+  `insure_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '质损保险状态(1-未完结,2-已完结)',
+  `date_id` int(4) DEFAULT NULL COMMENT '办理保险赔付统计时间',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '办理保险赔付时间',
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- ----------------------------
+-- Table structure for damage_insure_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `damage_insure_rel`;
+CREATE TABLE `damage_insure_rel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `insure_id` int(10) NOT NULL COMMENT '保险公司ID',
+  `damage_id` int(10) NOT NULL COMMENT '质损编号ID',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- ----------------------------
+-- Table structure for dp_route_load_task_clean_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `dp_route_load_task_clean_rel`;
+CREATE TABLE `dp_route_load_task_clean_rel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dp_route_load_task_id` int(10) NOT NULL COMMENT '路线任务ID',
+  `truck_id` int(10) NOT NULL COMMENT '货车ID',
+  `receive_id` int(10) NOT NULL COMMENT '经销商ID',
+  `single_price` decimal(10,2) DEFAULT NULL COMMENT '洗车单价',
+  `total_price` decimal(10,2) DEFAULT NULL COMMENT '洗车总价',
+  `grant_user_id` int(10) DEFAULT NULL COMMENT '发放人',
+  `drive_user_id` int(10) DEFAULT NULL COMMENT '领取人司机',
+  `date_id` int(4) DEFAULT NULL COMMENT '领取时间',
+  `collect_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '领取状态(1-未审核,2-已通过,3-未领取,4-已领取)',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
