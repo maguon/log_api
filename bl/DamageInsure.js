75215f0523466abacf90897140025d7b1af7d4b0
zwl123 <wenlong.zhang@myxxjs.com>
2018-01-29 16:13:29 +0800

    修改质损保险新增功能

--- bl/DamageInsure.js
+++ bl/DamageInsure.js
@@ -8,6 +8,7 @@ var resUtil = require('../util/ResponseUtil.js');
 var encrypt = require('../util/Encrypt.js');
 var listOfValue = require('../util/ListOfValue.js');
 var damageInsureDAO = require('../dao/DamageInsureDAO.js');
+var damageInsureRelDAO = require('../dao/DamageInsureRelDAO.js');
 var oAuthUtil = require('../util/OAuthUtil.js');
 var Seq = require('seq');
 var serverLogger = require('../util/ServerLogger.js');
@@ -15,15 +16,56 @@ var logger = serverLogger.createLogger('DamageInsure.js');
 
 function createDamageInsure(req,res,next){
     var params = req.params ;
-    damageInsureDAO.addDamageInsure(params,function(error,result){
-        if (error) {
-            logger.error(' createDamageInsure ' + error.message);
-            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
-        } else {
-            logger.info(' createDamageInsure ' + 'success');
-            resUtil.resetCreateRes(res,result,null);
-            return next();
-        }
+    var damageInsureId = 0;
+    Seq().seq(function(){
+        var that = this;
+        damageInsureDAO.addDamageInsure(params,function(error,result){
+            if (error) {
+                logger.error(' createDamageInsure ' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            } else {
+                if(result&&result.insertId>0){
+                    logger.info(' createDamageInsure ' + 'success');
+                    damageInsureId = result.insertId;
+                    that();
+                }else{
+                    resUtil.resetFailedRes(res,"create damageInsure failed");
+                    return next();
+                }
+            }
+        })
+    }).seq(function(){
+        var that = this;
+        var damageIds = params.damageIds;
+        var rowArray = [] ;
+        rowArray.length= damageIds.length;
+        Seq(rowArray).seqEach(function(rowObj,i){
+            var that = this;
+            var subParams ={
+                damageInsureId : damageInsureId,
+                damageId : damageIds[i],
+                row : i+1,
+            }
+            damageInsureRelDAO.addDamageInsureRel(subParams,function(err,result){
+                if (err) {
+                    logger.error(' createDamageInsureRel ' + err.message);
+                    throw sysError.InternalError(err.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+                } else {
+                    if(result&&result.insertId>0){
+                        logger.info(' createDamageInsureRel ' + 'success');
+                    }else{
+                        logger.warn(' createDamageInsureRel ' + 'failed');
+                    }
+                    that(null,i);
+                }
+            })
+        }).seq(function(){
+            that();
+        })
+    }).seq(function(){
+        logger.info(' createDamageInsure ' + 'success');
+        resUtil.resetCreateRes(res,{insertId:damageInsureId},null);
+        return next();
     })
 }
 
