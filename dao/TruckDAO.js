14b5aec56dc18f40450e412bf429a6eb1962cea1
zwl123 <wenlong.zhang@myxxjs.com>
2018-03-12 14:46:18 +0800

    增加关联司机电话字段

--- dao/TruckDAO.js
+++ dao/TruckDAO.js
@@ -49,7 +49,7 @@ function addTruckTrailer(params,callback){
 
 function getTruckFirst(params,callback) {
     var query = " select h.*,t.id as trail_id,t.truck_num as trail_num,t.number as trail_number," +
-        " b.brand_name,d.drive_name,d1.drive_name as vice_drive_name,c.company_name,c.operate_type " +
+        " b.brand_name,d.drive_name,d.tel,d1.drive_name as vice_drive_name,c.company_name,c.operate_type " +
         " from truck_info h left join truck_info t on h.rel_id = t.id " +
         " left join truck_brand b on h.brand_id = b.id  " +
         " left join drive_info d on h.drive_id = d.id  " +
@@ -125,7 +125,7 @@ function getTruckFirst(params,callback) {
 }
 
 function getTruckTrailer(params,callback) {
-    var query = " select h.*,t.id as first_id,t.truck_num as first_num,d.id as driveId,d.drive_name,c.company_name,c.operate_type,b.brand_name " +
+    var query = " select h.*,t.id as first_id,t.truck_num as first_num,d.id as driveId,d.drive_name,d.tel,c.company_name,c.operate_type,b.brand_name " +
         " from truck_info h left join truck_info t on h.id = t.rel_id " +
         " left join truck_brand b on h.brand_id = b.id  " +
         " left join drive_info d on t.drive_id = d.id  " +
