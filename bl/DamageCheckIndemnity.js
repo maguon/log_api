c123f8cf4d5dbbe935e5379862b4151b54388c3f
zwl123 <wenlong.zhang@myxxjs.com>
2018-04-04 17:01:06 +0800

    更新打款处理状态

--- bl/DamageCheckIndemnity.js
+++ bl/DamageCheckIndemnity.js
@@ -13,6 +13,7 @@ var damageCheckDAO = require('../dao/DamageCheckDAO.js');
 var oAuthUtil = require('../util/OAuthUtil.js');
 var Seq = require('seq');
 var serverLogger = require('../util/ServerLogger.js');
+var moment = require('moment/moment.js');
 var logger = serverLogger.createLogger('DamageCheckIndemnity.js');
 
 function createDamageCheckIndemnity(req,res,next){
