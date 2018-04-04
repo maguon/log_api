6354f6c2f5f2be576d181ad91cd993b85f07a45c
zwl123 <wenlong.zhang@myxxjs.com>
2018-04-04 10:00:34 +0800

    删除路线同时删除出车款关联

--- bl/DpRouteTask.js
+++ bl/DpRouteTask.js
@@ -12,6 +12,7 @@ var dpRouteTaskDAO = require('../dao/DpRouteTaskDAO.js');
 var dpRouteLoadTaskDAO = require('../dao/DpRouteLoadTaskDAO.js');
 var dpRouteLoadTaskDetailDAO = require('../dao/DpRouteLoadTaskDetailDAO.js');
 var truckDispatchDAO = require('../dao/TruckDispatchDAO.js');
+var dpRouteTaskLoanRelDAO = require('../dao/DpRouteTaskLoanRelDAO.js');
 var oAuthUtil = require('../util/OAuthUtil.js');
 var Seq = require('seq');
 var serverLogger = require('../util/ServerLogger.js');
@@ -337,6 +338,21 @@ function removeDpRouteTask(req,res,next){
                 }else{
                     that();
                 }
+            }
+        })
+    }).seq(function(){
+        var that = this;
+        dpRouteTaskLoanRelDAO.deleteDpRouteTaskLoanRelAll(params,function(error,result){
+            if (error) {
+                logger.error(' removeDpRouteTaskLoanRelAll ' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            } else {
+                if(result&&result.affectedRows>0){
+                    logger.info(' removeDpRouteTaskLoanRelAll ' + 'success');
+                }else{
+                    logger.warn(' removeDpRouteTaskLoanRelAll ' + 'failed');
+                }
+                that();
             }
         })
     }).seq(function () {
