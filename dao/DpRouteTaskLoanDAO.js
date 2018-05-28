f0ab8991fbc6c3f4b322cd10101373d37dcaed30
zwl123 <wenlong.zhang@myxxjs.com>
2018-05-28 17:10:49 +0800

    追加未申请出车款查询字段

--- dao/DpRouteTaskLoanDAO.js
+++ dao/DpRouteTaskLoanDAO.js
@@ -275,13 +275,41 @@ function getDpRouteTaskLoanDayStat(params,callback){
 }
 
 function getDpRouteTaskNotLoan(params,callback) {
-    var query = " select dpr.* from dp_route_task dpr " +
+    var query = " select dpr.*,c.city_name as city_route_start,ce.city_name as city_route_end, " +
+        " d.drive_name,t.truck_num,sum(dprl.plan_count) as plan_count,sum(dprl.real_count) as real_count " +
+        "  from dp_route_task dpr " +
         " left join dp_route_task_loan_rel rel on dpr.id = rel.dp_route_task_id " +
+        " left join city_info c on dpr.route_start_id = c.id " +
+        " left join city_info ce on dpr.route_end_id = ce.id " +
+        " left join drive_info d on dpr.drive_id = d.id " +
+        " left join truck_info t on dpr.truck_id = t.id " +
+        " left join dp_route_load_task dprl on dpr.id = dprl.dp_route_task_id " +
         " where rel.dp_route_task_id is null ";
     var paramsArray=[],i=0;
+    if(params.dpRouteTaskId){
+        paramsArray[i++] = params.dpRouteTaskId;
+        query = query + " and dpr.id = ? ";
+    }
+    if(params.truckId){
+        paramsArray[i++] = params.truckId;
+        query = query + " and dpr.truck_id = ? ";
+    }
+    if(params.truckNum){
+        paramsArray[i++] = params.truckNum;
+        query = query + " and t.truck_num = ? ";
+    }
+    if(params.driveId){
+        paramsArray[i++] = params.driveId;
+        query = query + " and dpr.drive_id = ? ";
+    }
+    if(params.driveName){
+        paramsArray[i++] = params.driveName;
+        query = query + " and d.drive_name = ? ";
+    }
     if(params.taskStatusArr){
         query = query + " and dpr.task_status not in ("+params.taskStatusArr + ") "
     }
+    query = query + ' group by dpr.id ';
     if (params.start && params.size) {
         paramsArray[i++] = parseInt(params.start);
         paramsArray[i++] = parseInt(params.size);
