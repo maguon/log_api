4cf932b5f97cbe13ee606a0435fce015e874223e
zwl123 <wenlong.zhang@myxxjs.com>
2018-06-08 09:10:50 +0800

    交接单增加字段

--- dao/SettleHandoverDAO.js
+++ dao/SettleHandoverDAO.js
@@ -7,11 +7,12 @@ var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('SettleHandoverDAO.js');
 
 function addSettleHandover(params,callback){
-    var query = " insert into settle_handover_info (number,entrust_id,op_user_id,remark) values ( ? , ? , ? , ? )";
+    var query = " insert into settle_handover_info (number,entrust_id,op_user_id,received_date,remark) values ( ? , ? , ? , ? , ? )";
     var paramsArray=[],i=0;
     paramsArray[i++]=params.number;
     paramsArray[i++]=params.entrustId;
     paramsArray[i++]=params.userId;
+    paramsArray[i++]=params.receivedDate;
     paramsArray[i]=params.remark;
     db.dbQuery(query,paramsArray,function(error,rows){
         logger.debug(' addSettleHandover ');
@@ -20,17 +21,37 @@ function addSettleHandover(params,callback){
 }
 
 function getSettleHandover(params,callback) {
-    var query = " select * from settle_handover_info where id is not null ";
+    var query = " select sh.*,e.short_name,u.real_name as op_user_name from settle_handover_info sh" +
+        " left join entrust_info e on sh.entrust_id = e.id " +
+        " left join user_info u on sh.op_user_id = u.uid " +
+        " where sh.id is not null ";
     var paramsArray=[],i=0;
     if(params.settleHandoverId){
         paramsArray[i++] = params.settleHandoverId;
-        query = query + " and id = ? ";
+        query = query + " and sh.id = ? ";
     }
     if(params.number){
         paramsArray[i++] = params.number;
-        query = query + " and number = ? ";
+        query = query + " and sh.number = ? ";
     }
-    query = query + ' order by id desc ';
+    if(params.entrustId){
+        paramsArray[i++] = params.entrustId;
+        query = query + " and sh.entrust_id = ? ";
+    }
+    if(params.receivedDateStart){
+        paramsArray[i++] = params.receivedDateStart +" 00:00:00";
+        query = query + " and sh.received_date >= ? ";
+    }
+    if(params.receivedDateEnd){
+        paramsArray[i++] = params.receivedDateEnd +" 23:59:59";
+        query = query + " and sh.received_date <= ? ";
+    }
+    query = query + ' order by sh.id desc ';
+    if (params.start && params.size) {
+        paramsArray[i++] = parseInt(params.start);
+        paramsArray[i] = parseInt(params.size);
+        query += " limit ? , ? "
+    }
     db.dbQuery(query,paramsArray,function(error,rows){
         logger.debug(' getSettleHandover ');
         return callback(error,rows);
