7f378316fb722b36450e7ff2caf1eee323c6ec69
zwl123 <wenlong.zhang@myxxjs.com>
2018-06-26 16:49:26 +0800

    修正新增调度参数

--- dao/DpRouteTaskDAO.js
+++ dao/DpRouteTaskDAO.js
@@ -9,7 +9,7 @@ var logger = serverLogger.createLogger('DpRouteTaskDAO.js');
 
 function addDpRouteTask(params,callback){
     var query = " insert into dp_route_task (user_id,truck_id,drive_id,route_start_id,route_end_id,distance,task_plan_date) " +
-        " values ( ? , ? , ? , ? , ? , ? , ? , ? ) ";
+        " values ( ? , ? , ? , ? , ? , ? , ? ) ";
     var paramsArray=[],i=0;
     paramsArray[i++]=params.userId;
     paramsArray[i++]=params.truckId;
