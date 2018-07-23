15bfa400ed3e5203a6f138d9f85fdd9d71220d23
zwl123 <wenlong.zhang@myxxjs.com>
2018-07-23 08:49:20 +0800

    修改经销商类型默认值

--- db/sql/0.0.8/update_base.sql
+++ db/sql/0.0.8/update_base.sql
@@ -2,4 +2,4 @@
 -- 2018-07-19 更新
 -- ----------------------------
 ALTER TABLE `receive_info`
-ADD COLUMN `receive_type`  tinyint(1) NULL COMMENT '经销商类型(1-4S店,2-大客户,3-临时停放地)' AFTER `receive_name`;
+ADD COLUMN `receive_type`  tinyint(1) NULL DEFAULT 1 COMMENT '经销商类型(1-4S店,2-大客户,3-临时停放地)' AFTER `receive_name`;
