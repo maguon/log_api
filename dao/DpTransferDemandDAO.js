a2d9daa35f5703cca647d1a1ec2cfd967fcd0625
zwl123 <wenlong.zhang@myxxjs.com>
2018-07-26 15:40:50 +0800

    追加中转站原始装车地

--- dao/DpTransferDemandDAO.js
+++ dao/DpTransferDemandDAO.js
@@ -27,10 +27,11 @@ function addDpTransferDemand(params,callback){
 }
 
 function getDpTransferDemand(params,callback) {
-    var query = " select dptd.*,c1.city_name as route_start_name,c2.city_name as route_end_name,r.short_name" +
+    var query = " select dptd.*,c1.city_name as route_start_name,ba.addr_name,c2.city_name as route_end_name,r.short_name" +
         " from dp_transfer_demand_info dptd" +
         " left join city_info c1 on dptd.route_start_id = c1.id " +
         " left join city_info c2 on dptd.route_end_id = c2.id " +
+        " left join base_addr ba on dptd.base_addr_id = ba.id " +
         " left join receive_info r on dptd.receive_id = r.id " +
         " where dptd.id is not null ";
     var paramsArray=[],i=0;
--- db/sql/0.0.8/update_base.sql
+++ db/sql/0.0.8/update_base.sql
@@ -9,7 +9,8 @@ ADD COLUMN `receive_type`  tinyint(1) NULL DEFAULT 1 COMMENT '经销商类型(1-
 ALTER TABLE `settle_handover_info`
 ADD COLUMN `serial_number`  int(10) NOT NULL DEFAULT 0 COMMENT '序号' AFTER `number`;
 -- ----------------------------
--- 2018-07-24 更新 调度中转
+-- 2018-07-24 更新
+-- 调度中转添加字段
 -- ----------------------------
 ALTER TABLE `dp_route_load_task`
 ADD COLUMN `transfer_flag`  tinyint(1) NULL DEFAULT 0 COMMENT '是否中转标识(0-否,1-是)' AFTER `load_task_status`,
@@ -25,6 +26,7 @@ DROP TABLE IF EXISTS `dp_transfer_demand_info`;
 CREATE TABLE `dp_transfer_demand_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '操作人ID',
+  `demand_id` int(10) NOT NULL DEFAULT '0' COMMENT '调度需求ID',
   `route_start_id` int(10) NOT NULL DEFAULT '0' COMMENT '起始地ID',
   `base_addr_id` int(10) NOT NULL DEFAULT '0' COMMENT '起始地发货地址ID',
   `transfer_city_id` int(10) NOT NULL DEFAULT '0' COMMENT '中转站ID',
@@ -52,7 +54,7 @@ CREATE TABLE `dp_task_transfer_stat` (
   `route_end_id` int(10) NOT NULL DEFAULT '0' COMMENT '目的地ID',
   `receive_id` int(10) NOT NULL DEFAULT '0' COMMENT '经销商ID',
   `transfer_count` int(10) NOT NULL DEFAULT '0' COMMENT '中转数',
-  `plan_count` int(10) NOT NULL DEFAULT '0' COMMENT '已派发台数',
+  `plan_count` int(10) NULL DEFAULT '0' COMMENT '已派发台数',
   `date_id` int(4) NOT NULL COMMENT '指令时间',
   `transfer_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '中转状态(1-未完成,2-已完成)',
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -60,4 +62,52 @@ CREATE TABLE `dp_task_transfer_stat` (
   PRIMARY KEY (`route_start_id`,`base_addr_id`,`transfer_city_id`,`transfer_addr_id`,`route_end_id`,`receive_id`,`date_id`),
   UNIQUE KEY `id` (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- ----------------------------
+-- 2018-07-25 更新
+-- 追加transfer_flag状态验证，如果任务是中转，更新dp_task_stat中转数
+-- ----------------------------
+DROP TRIGGER IF EXISTS `trg_new_load_task`;
+DELIMITER ;;
+CREATE TRIGGER `trg_new_load_task` AFTER INSERT ON `dp_route_load_task` FOR EACH ROW BEGIN
+UPDATE dp_demand_info set plan_count=plan_count+new.plan_count where id= new.demand_id ;
+UPDATE dp_task_stat set plan_count = plan_count+new.plan_count where route_start_id=new.route_start_id
+and base_addr_id=new.base_addr_id and route_end_id = new.route_end_id and receive_id=new.receive_id and date_id = new.date_id;
+IF(new.transfer_flag=1) THEN
+UPDATE dp_task_stat set transfer_count = transfer_count+new.plan_count where route_start_id=new.route_start_id
+and base_addr_id=new.base_addr_id and route_end_id = new.route_end_id and receive_id=new.receive_id and date_id = new.date_id;
+END IF;
+END
+;;
+DELIMITER ;
+-- ----------------------------
+-- 2018-07-25 更新
+-- 追加transfer_flag状态验证，如果中转任务被取消load_task_status=8，更新dp_task_stat中转数
+-- ----------------------------
+DROP TRIGGER IF EXISTS `trg_update_load_task`;
+DELIMITER ;;
+CREATE TRIGGER `trg_update_load_task` AFTER UPDATE ON `dp_route_load_task` FOR EACH ROW BEGIN
+IF(new.load_task_status=8 && old.load_task_status<>8 && old.transfer_flag =1) THEN
+UPDATE dp_demand_info set plan_count=plan_count-old.plan_count where id= new.demand_id ;
+UPDATE dp_task_stat set plan_count = plan_count-old.plan_count,transfer_count = transfer_count -old.plan_count
+where route_start_id=new.route_start_id
+and base_addr_id=new.base_addr_id and route_end_id = new.route_end_id and receive_id=new.receive_id and date_id = new.date_id;
+ELSEIF(new.load_task_status=8 && old.load_task_status<>8) THEN
+UPDATE dp_demand_info set plan_count=plan_count-old.plan_count where id= new.demand_id ;
+UPDATE dp_task_stat set plan_count = plan_count-old.plan_count
+where route_start_id=new.route_start_id
+and base_addr_id=new.base_addr_id and route_end_id = new.route_end_id and receive_id=new.receive_id and date_id = new.date_id;
+END IF;
+IF(new.load_task_status=3 && old.load_task_status<>3) THEN
+UPDATE dp_route_task set task_status=4,car_count = (select sum(real_count)
+from dp_route_load_task where dp_route_task_id = old.dp_route_task_id)
+where (select count(*) from dp_route_load_task where load_task_status <>3 and load_task_status<>8 and dp_route_task_id = old.dp_route_task_id ) =0 and id= old.dp_route_task_id;
+END IF;
+IF(new.load_task_status=7 && old.load_task_status<>7) THEN
+UPDATE dp_route_task set task_status=10
+where id = old.dp_route_task_id and task_status =9 and
+(select count(*) from dp_route_load_task where load_task_status <>7 and load_task_status<>8 and dp_route_task_id = old.dp_route_task_id ) =0 ;
+END IF;
+END
+;;
+DELIMITER ;
 
