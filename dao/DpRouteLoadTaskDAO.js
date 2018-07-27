9f035ee576cdb071b575678abf7c26838c93e0d0
zwl123 <wenlong.zhang@myxxjs.com>
2018-07-27 15:26:19 +0800

    新增任务追加中转需求参数

--- dao/DpRouteLoadTaskDAO.js
+++ dao/DpRouteLoadTaskDAO.js
@@ -7,11 +7,12 @@ var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('DpRouteLoadTaskDAO.js');
 
 function addDpRouteLoadTask(params,callback){
-    var query = " insert into dp_route_load_task (user_id,demand_id,dp_route_task_id,route_start_id,base_addr_id,route_end_id,receive_id,date_id," +
-        "plan_date,plan_count,transfer_flag,transfer_city_id,transfer_addr_id) values ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? ) ";
+    var query = " insert into dp_route_load_task (user_id,demand_id,transfer_demand_id,dp_route_task_id,route_start_id,base_addr_id,route_end_id,receive_id,date_id," +
+        "plan_date,plan_count,transfer_flag,transfer_city_id,transfer_addr_id) values ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? ) ";
     var paramsArray=[],i=0;
     paramsArray[i++]=params.userId;
     paramsArray[i++]=params.dpDemandId;
+    paramsArray[i++]=params.transferDemandId;
     paramsArray[i++]=params.dpRouteTaskId;
     paramsArray[i++]=params.routeStartId;
     paramsArray[i++]=params.baseAddrId;
