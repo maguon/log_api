c3769ba1ce9b0a96018e73ef1ea4f451f34d3f8a
zwl123 <wenlong.zhang@myxxjs.com>
2018-07-27 08:35:07 +0800

    追加中转字段

--- dao/DpRouteLoadTaskDAO.js
+++ dao/DpRouteLoadTaskDAO.js
@@ -32,7 +32,7 @@ function addDpRouteLoadTask(params,callback){
 function getDpRouteLoadTask(params,callback) {
     var query = " select dprl.*,u.real_name as task_op_name,u1.real_name as field_op_name,ba.addr_name,c.city_name as city_start_name,c1.city_name,r.short_name,r.clean_fee,dpd.pre_count, " +
         " dpr.truck_id,dpr.drive_id,dpr.task_plan_date,dpr.task_start_date,dpr.date_id as task_end_date,t.truck_num,d.drive_name,u2.mobile,count(dpdtl.id) as car_count, " +
-        " count(case when cer.exception_status = 1 then cer.id end) as car_exception_count " +
+        " count(case when cer.exception_status = 1 then cer.id end) as car_exception_count,c2.city_name as transfer_city_name,ba1.addr_name as transfer_addr_name " +
         " from dp_route_load_task dprl " +
         " left join dp_demand_info dpd on dprl.demand_id = dpd.id " +
         " left join user_info u on dprl.user_id = u.uid " +
@@ -43,8 +43,10 @@ function getDpRouteLoadTask(params,callback) {
         " left join truck_info t on dpr.truck_id = t.id " +
         " left join drive_info d on dpr.drive_id = d.id " +
         " left join base_addr ba on dprl.base_addr_id = ba.id " +
+        " left join base_addr ba1 on dprl.transfer_addr_id = ba1.id " +
         " left join city_info c on dprl.route_start_id = c.id " +
         " left join city_info c1 on dprl.route_end_id = c1.id " +
+        " left join city_info c2 on dprl.transfer_city_id = c2.id" +
         " left join receive_info r on dprl.receive_id = r.id " +
         " left join user_info u2 on d.user_id = u2.uid " +
         " where dprl.load_task_status !=8 and dprl.id is not null ";
