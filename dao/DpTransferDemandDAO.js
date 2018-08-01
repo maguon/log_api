c91faef5c7ac5f889f19a2f93938e666c70183a9
zwl123 <wenlong.zhang@myxxjs.com>
2018-08-01 10:00:45 +0800

    追加中转需求查询结果

--- dao/DpTransferDemandDAO.js
+++ dao/DpTransferDemandDAO.js
@@ -27,11 +27,15 @@ function addDpTransferDemand(params,callback){
 }
 
 function getDpTransferDemand(params,callback) {
-    var query = " select dptd.*,c1.city_name as route_start_name,ba.addr_name,c2.city_name as route_end_name,r.short_name" +
-        " from dp_transfer_demand_info dptd" +
+    var query = " select dptd.*,c1.city_name as route_start_name,ba.addr_name," +
+        " c3.city_name as transfer_city_name,ba1.addr_name as transfer_addr_name, " +
+        " c2.city_name as route_end_name,r.short_name" +
+        " from dp_transfer_demand_info dptd " +
         " left join city_info c1 on dptd.route_start_id = c1.id " +
         " left join city_info c2 on dptd.route_end_id = c2.id " +
+        " left join city_info c3 on dptd.transfer_city_id = c3.id " +
         " left join base_addr ba on dptd.base_addr_id = ba.id " +
+        " left join base_addr ba1 on dptd.transfer_addr_id = ba1.id " +
         " left join receive_info r on dptd.receive_id = r.id " +
         " where dptd.id is not null ";
     var paramsArray=[],i=0;
