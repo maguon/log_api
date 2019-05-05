33086b69a57e9c647cc7f44407c3725d8b9ed10f
zwl123 <wenlong.zhang@myxxjs.com>
2019-05-05 16:43:13 +0800

    追加备注

--- dao/DriveExceedOilDateDAO.js
+++ dao/DriveExceedOilDateDAO.js
@@ -7,11 +7,12 @@ var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('DriveExceedOilDateDAO.js');
 
 function addDriveExceedOilDate(params,callback){
-    var query = " insert into drive_exceed_oil_date (month_date_id,drive_id,truck_id) values ( ? , ? , ? )";
+    var query = " insert into drive_exceed_oil_date (month_date_id,drive_id,truck_id,remark) values ( ? , ? , ? , ? )";
     var paramsArray=[],i=0;
     paramsArray[i++]=params.monthDateId;
     paramsArray[i++]=params.driveId;
     paramsArray[i++]=params.truckId;
+    paramsArray[i++]=params.remark;
     db.dbQuery(query,paramsArray,function(error,rows){
         logger.debug(' addDriveExceedOilDate ');
         return callback(error,rows);
@@ -53,9 +54,10 @@ function getDriveExceedOilDate(params,callback) {
 }
 
 function updateDriveExceedOilDate(params,callback){
-    var query = " update drive_exceed_oil_date set actual_money = ? where id = ? " ;
+    var query = " update drive_exceed_oil_date set actual_money = ? , remark = ? where id = ? " ;
     var paramsArray=[],i=0;
     paramsArray[i++]=params.actualMoney;
+    paramsArray[i++]=params.remark;
     paramsArray[i]=params.exceedOilDateId;
     db.dbQuery(query,paramsArray,function(error,rows){
         logger.debug(' updateDriveExceedOilDate ');
