0a19308f9668382367a085429110c6b1884af125
zwl123 <wenlong.zhang@myxxjs.com>
2019-06-05 11:19:26 +0800

    修改司机工资查询sql

--- dao/DriveSalaryDAO.js
+++ dao/DriveSalaryDAO.js
@@ -63,7 +63,7 @@ function getDriveSalary(params,callback) {
         " left join (select dpr.drive_id, " +
         " sum( case when dprl.receive_flag=0 and dprl.transfer_flag=0 then dpr.car_count end)*4 as enter_fee " +
         " from dp_route_load_task dprl " +
-        " left join  dp_route_task dpr on dprl.dp_route_task_id = dpr.id " +
+        " left join dp_route_task dpr on dprl.dp_route_task_id = dpr.id " +
         " where dpr.task_plan_date>="+params.monthDateId+"01 and dpr.task_plan_date<="+params.monthDateId+"31 and dpr.task_status>=9" +
         " group by dpr.drive_id) dprm on dprtm.drive_id = dprm.drive_id " +
         " where dprtm.drive_id is not null ";
