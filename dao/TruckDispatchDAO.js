a4b093e739a3f472e151e7f5b5b0564fc7a82148
zwl123 <wenlong.zhang@myxxjs.com>
2019-06-26 16:20:42 +0800

    修改查询所属类型

--- dao/TruckDispatchDAO.js
+++ dao/TruckDispatchDAO.js
@@ -9,7 +9,7 @@ var logger = serverLogger.createLogger('TruckDispatchDAO.js');
 function getTruckDispatch(params,callback) {
     var query = " select td.*,ci.city_name,cs.city_name as task_start_name,ce.city_name as task_end_name, " +
         " h.truck_num,h.hp,h.truck_tel,h.drive_id,h.company_id,h.truck_type, " +
-        " d.drive_name,u.mobile,c.company_name,c.operate_type from truck_dispatch td " +
+        " d.drive_name,u.mobile,c.company_name,h.operate_type from truck_dispatch td " +
         " left join city_info ci on td.current_city = ci.id " +
         " left join city_info cs on td.task_start = cs.id " +
         " left join city_info ce on td.task_end = ce.id " +
