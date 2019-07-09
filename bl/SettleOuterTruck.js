026c6d8e9bd184d9a2909052bdba7954d5b95cd6
zwl123 <wenlong.zhang@myxxjs.com>
2019-07-09 15:15:53 +0800

    追加外协结算导出

--- bl/SettleOuterTruck.js
+++ bl/SettleOuterTruck.js
@@ -88,6 +88,69 @@ function updateSettleOuterTruck(req,res,next){
     })
 }
 
+function getSettleOuterTruckCsv(req,res,next){
+    var csvString = "";
+    var header = "VIN"+ ',' +"品牌"+ ',' +"调度编号"+ ',' +"司机"+ ',' +"货车牌号"+ ',' +"始发城市"+ ','+"装车地点"+ ','+"目的城市"+ ','+"经销商"+ ','+"指令时间"+ ','+
+        "公里数(公里)"+ ','+"价格/公里"+ ','+"金额";
+    csvString = header + '\r\n'+csvString;
+    var params = req.params ;
+    var parkObj = {};
+    var fees = 0;
+    settleOuterTruckDAO.getSettleOuterTruckList(params,function(error,rows){
+        if (error) {
+            logger.error(' getSettleOuterTruckList ' + error.message);
+            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+        } else {
+            for(var i=0;i<rows.length;i++){
+                parkObj.vin = rows[i].vin;
+                parkObj.makeName = rows[i].make_name;
+                parkObj.id = rows[i].id;
+                parkObj.driveName = rows[i].drive_name;
+                parkObj.truckNum = rows[i].truck_num;
+                parkObj.routeStart = rows[i].route_start;
+                parkObj.addrName = rows[i].addr_name;
+                parkObj.routeEnd = rows[i].route_end;
+                if(rows[i].r_short_name == null){
+                    parkObj.rShortName = "";
+                }else{
+                    parkObj.rShortName = rows[i].r_short_name;
+                }
+                if(rows[i].order_date == null){
+                    parkObj.orderDate = "";
+                }else{
+                    parkObj.orderDate = new Date(rows[i].order_date).toLocaleDateString();
+                }
+                if(rows[i].distance == null){
+                    parkObj.distance = "";
+                }else{
+                    parkObj.distance = rows[i].distance;
+                }
+                if(rows[i].fee == null){
+                    parkObj.fee = "";
+                }else{
+                    parkObj.fee = rows[i].fee;
+                }
+                fees = rows[i].distance*rows[i].fee;
+                if(fees == 0){
+                    parkObj.fees = "";
+                }else{
+                    parkObj.fees = fees;
+                }
+                csvString = csvString+parkObj.vin+","+parkObj.makeName+","+parkObj.id+","+parkObj.driveName+","+parkObj.truckNum+","+
+                    parkObj.routeStart+","+parkObj.addrName+","+parkObj.routeEnd+","+parkObj.rShortName+","+parkObj.orderDate+","+
+                    parkObj.distance+","+parkObj.fee+","+parkObj.fees+","+'\r\n';
+            }
+            var csvBuffer = new Buffer(csvString,'utf8');
+            res.set('content-type', 'application/csv');
+            res.set('charset', 'utf8');
+            res.set('content-length', csvBuffer.length);
+            res.writeHead(200);
+            res.write(csvBuffer);//TODO
+            res.end();
+            return next(false);
+        }
+    })
+}
 
 
 module.exports = {
@@ -95,5 +158,6 @@ module.exports = {
     querySettleOuterTruck : querySettleOuterTruck,
     querySettleOuterTruckList : querySettleOuterTruckList,
     querySettleOuterTruckCarCount : querySettleOuterTruckCarCount,
-    updateSettleOuterTruck : updateSettleOuterTruck
+    updateSettleOuterTruck : updateSettleOuterTruck,
+    getSettleOuterTruckCsv : getSettleOuterTruckCsv
 }
