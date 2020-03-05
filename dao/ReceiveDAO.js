7fb94888e6469da7e30fc04b55028999ff4bc339
yjyvsdream <jingyuan.yu@myxxjs.com>
2020-03-05 09:27:06 +0800

    修改接口：修改sql  bug

--- dao/ReceiveDAO.js
+++ dao/ReceiveDAO.js
@@ -50,7 +50,7 @@ function getReceive(params,callback) {
     }
     if(params.cityId){
         paramsArray[i++] = params.cityId;
-        query = query + " and re.city_id = ? or re.city_id = 0 ";
+        query = query + " and (re.city_id = ? or re.city_id = 0) ";
     }
     if(params.makeId){
         paramsArray[i++] = params.makeId;
