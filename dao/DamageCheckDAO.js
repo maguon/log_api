06a8b140d67bf3907c6fcd5a6c301de6820738b6
yingmei567 <yingmei.yan@myxxjs.com>
2021-05-24 12:56:29 +0800

    更新赔款状态时同时更新赔款时间和质损状态

--- dao/DamageCheckDAO.js
+++ dao/DamageCheckDAO.js
@@ -136,8 +136,19 @@ function updateDamageCheckFinishTime(params,callback){
     });
 }
 
+function updateDamageCheckDateId(params,callback){
+    var query = " update damage_check set date_id = ? where id = ? " ;
+    var paramsArray=[],i=0;
+    paramsArray[i++]=params.dateId;
+    paramsArray[i]=params.damageCheckId;
+    db.dbQuery(query,paramsArray,function(error,rows){
+        logger.debug(' updateDamageCheckDateId ');
+        return callback(error,rows);
+    });
+}
+
 function updateDamageCheckIndemnityStatus(params,callback){
-    var query = " update damage_check set damage_indemnity_status = ? where id = ? " ;
+    var query = " update damage_check set damage_indemnity_status = ? where id = ? and damage_indemnity_status=1" ;
     var paramsArray=[],i=0;
     paramsArray[i++]=params.damageIndemnityStatus;
     paramsArray[i]=params.damageCheckId;
@@ -258,6 +269,7 @@ module.exports ={
     updateScPayment : updateScPayment,
     updateScPaymentByDamageId : updateScPaymentByDamageId,
     updateDamageCheckFinishTime : updateDamageCheckFinishTime,
+    updateDamageCheckDateId : updateDamageCheckDateId,
     updateDamageCheckIndemnityStatus : updateDamageCheckIndemnityStatus,
     getDamageCheckMonthStat  : getDamageCheckMonthStat,
     getDamageCheckWeekStat : getDamageCheckWeekStat,
--- dao/DamageDAO.js
+++ dao/DamageDAO.js
@@ -466,6 +466,18 @@ function updateDamageStatus(params,callback){
     });
 }
 
+function updateDamageStatusCheckId(params,callback){
+    var query = " UPDATE damage_info SET damage_status = ?" +
+        " WHERE id = ( SELECT damage_id FROM damage_check WHERE id = ? ) " ;
+    var paramsArray=[],i=0;
+    paramsArray[i++]=params.damageStatus;
+    paramsArray[i]=params.damageCheckId;
+    db.dbQuery(query,paramsArray,function(error,rows){
+        logger.debug(' updateDamageStatusCheckId ');
+        return callback(error,rows);
+    });
+}
+
 function updateDamageHangStatus(params,callback){
     var query = " update damage_info set hang_status = ? where id = ? " ;
     var paramsArray=[],i=0;
@@ -798,6 +810,7 @@ module.exports ={
     getDamageTotalCost : getDamageTotalCost,
     updateDamage : updateDamage,
     updateDamageStatus : updateDamageStatus,
+    updateDamageStatusCheckId : updateDamageStatusCheckId,
     updateDamageHangStatus : updateDamageHangStatus,
     updateDamageStatStatus : updateDamageStatStatus,
     getDamageTypeMonthStat : getDamageTypeMonthStat,
