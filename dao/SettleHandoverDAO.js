858bc6360ccee5b697418966433f7deb32a58bf7
yingmei567 <yingmei.yan@myxxjs.com>
2021-06-15 16:20:43 +0800

    去掉计算工资添加距离条件

--- dao/SettleHandoverDAO.js
+++ dao/SettleHandoverDAO.js
@@ -419,8 +419,7 @@ function getDriveSettle(params,callback) {
         " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count=6 then drt.distance*0.8 " +
         " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count=7 then drt.distance*0.9 " +
         " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count=8 then drt.distance " +
-        " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count>=9 and distance<500 then drt.distance*1.4 " +
-        " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count>=9 and distance>=500 then drt.distance*1.2 " +
+        " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count>=9 then drt.distance*1.4 " +
         " end) distance_salary, " +
         " sum(case when drt.reverse_flag=1 then drt.reverse_money end) reverse_salary" +
         " from dp_route_task drt " +
@@ -487,8 +486,7 @@ function getDriveSettleSalary(params,callback) {
         " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count=6 then drt.distance*0.8 " +
         " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count=7 then drt.distance*0.9 " +
         " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count=8 then drt.distance " +
-        " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count>=9 and distance<500 then drt.distance*1.4 " +
-        " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count>=9 and distance>=500 then drt.distance*1.2 " +
+        " when drt.reverse_flag=0 and drt.truck_number=8 and drt.car_count>=9 then drt.distance*1.4 " +
         " end) distance_salary, " +
         " sum(case when drt.reverse_flag=1 then drt.reverse_money end) reverse_salary, " +
         " sum( CASE WHEN drt.load_flag = 1 THEN drt.distance END ) AS load_distance, " +
